754ce3523dd9ed04bda2a2d46c0aa7095a7f8ae2
y-rinsaka <yrinsaka@gmail.com>
2022-09-09 15:23:37 +0900

    Detail Page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,13 +9,24 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     /**
- * Post一覧を表示する
- * 
- * @param Post Postモデル
- * @return array Postモデルリスト
- */
-        public function index(Post $post)
+     * Post一覧を表示する
+     * 
+     * @param Post Postモデル
+     * @return array Postモデルリスト
+     */
+    public function index(Post $post)
     {
         return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+    }
+    /**
+     * 特定IDのpostを表示する
+     *
+     * @params Object Post // 引数の$postはid=1のPostインスタンス!!(関数実行でPostインスタンス自動生成)
+     * @return Reposnse post view
+     */
+     //postsディレクトリ下にあるshow.blade.phpを返す。post
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
     }
 }
